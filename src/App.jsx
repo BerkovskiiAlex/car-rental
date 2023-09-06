18dd99d009082fcede740916dab1a61f66beb44f
BerkovskiiAlex <berkovskii845@gmail.com>
2023-09-06 03:26:52 +0300

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello World!</header>
+    <div>
+      <header>good bye</header>
     </div>
   );
 }
