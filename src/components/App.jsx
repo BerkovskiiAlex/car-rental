4224821e101d3544b63ec072d368aee914cedcfe
BerkovskiiAlex <berkovskii845@gmail.com>
2023-09-06 16:16:28 +0300

    create nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 /** @format */
 
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout/Layout";
+import { Home } from "../pages/HomePage/Home";
+import { Catalog } from "../pages/CatalogPage/Catalog";
+import { Favorites } from "../pages/FavoritesPage/Favorites";
+
 function App() {
   return (
-    <div>
-      <header>Hello!</header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+export const Layout = () => {
+  const pages = [
+    { title: "Home", src: "/" },
+    { title: "Catalog", src: "/catalog" },
+    { title: "Favorites", src: "/favorites" },
+  ];
+  return (
+    <div>
+      <nav>
+        <ul>
+          {pages.map((page) => (
+            <li key={page.title}>
+              <NavLink to={page.src}>{page.title}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
