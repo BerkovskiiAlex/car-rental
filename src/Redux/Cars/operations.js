89c9bf7f6ad7f57172db51b504309b1ea2f2e890
BerkovskiiAlex <berkovskii845@gmail.com>
2023-09-10 20:52:10 +0300

    switched to another API

--- src/Redux/Cars/operations.js
+++ src/Redux/Cars/operations.js
@@ -4,14 +4,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const carsInstance = axios.create({
-  baseURL: "https://64de2222825d19d9bfb22657.mockapi.io",
+  baseURL: "https://64fdfe88596493f7af7ed7a0.mockapi.io/",
 });
 
 export const fetchCarsThunk = createAsyncThunk(
   "fetchCars",
   async (_, thunkAPI) => {
     try {
-      const res = await carsInstance.get(`/advert?limit=8&page=1`);
+      const res = await carsInstance.get(`/car-rental?limit=8&page=1`);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -23,7 +23,9 @@ export const loadMoreThunk = createAsyncThunk(
   "fetchLoadMore",
   async (currentPage, thunkAPI) => {
     try {
-      const res = await carsInstance.get(`/advert?limit=8&page=${currentPage}`);
+      const res = await carsInstance.get(
+        `/car-rental?limit=8&page=${currentPage}`
+      );
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
